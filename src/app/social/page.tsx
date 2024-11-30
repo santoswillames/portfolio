08cc37e253129402679a987af8a69f283e384708
willamessantos <willamessantos@live.com>
2024-11-29 23:30:49 -0300

    create social media page

---
+++ src/app/social/page.tsx
@@ -0,0 +1,86 @@
+import { ToggleTheme } from '@/components/theme/toggle'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Button } from '@/components/ui/button'
+import type { Metadata } from 'next'
+import Link from 'next/link'
+
+export const metadata: Metadata = {
+  title: 'Social',
+}
+
+export default function Social() {
+  return (
+    <main className="h-screen bg-muted flex flex-col w-full px-2">
+      <header className="w-full py-4 flex justify-end items-center">
+        <ToggleTheme />
+      </header>
+      <section className="flex flex-col justify-center items-center gap-1 w-full flex-1">
+        <Avatar className="w-24 h-24">
+          <AvatarImage src="https://github.com/santoswillames.png" />
+          <AvatarFallback>
+            <div className="bg-muted-foreground w-full h-full" />
+          </AvatarFallback>
+        </Avatar>
+        <h1 className="text-2xl font-semibold tracking-tight">
+          Willames Santos
+        </h1>
+        <p className="text-muted-foreground">
+          FullStack Developer & Content Creator
+        </p>
+        <div className="mt-6 justify-center items-center w-full flex flex-col gap-4">
+          <Button
+            asChild
+            className="w-full max-w-[348px] py-6 bg-muted-foreground"
+            size="lg"
+          >
+            <Link href="/store" target="_blank">
+              Equipamentos - Loja
+            </Link>
+          </Button>
+          <Button
+            asChild
+            className="w-full max-w-[348px] py-6 bg-muted-foreground"
+            size="lg"
+          >
+            <Link
+              href="https://www.youtube.com/@DevWillChannel?sub_confirmation=1"
+              target="_blank"
+            >
+              YouTube
+            </Link>
+          </Button>
+          <Button
+            asChild
+            className="w-full max-w-[348px] py-6 bg-muted-foreground"
+            size="lg"
+          >
+            <Link
+              href="https://www.linkedin.com/in/willames-silva-dos-santos-a26172126/"
+              target="_blank"
+            >
+              LinkedIn
+            </Link>
+          </Button>
+          <Button
+            asChild
+            className="w-full max-w-[348px] py-6 bg-muted-foreground"
+            size="lg"
+          >
+            <Link href="https://github.com/santoswillames" target="_blank">
+              GitHub
+            </Link>
+          </Button>
+          <Button
+            asChild
+            className="w-full max-w-[348px] py-6 bg-muted-foreground"
+            size="lg"
+          >
+            <Link href="https://twitter.com/dev_willl" target="_blank">
+              X- Twitter
+            </Link>
+          </Button>
+        </div>
+      </section>
+    </main>
+  )
+}
---
+++ src/app/store/page.tsx
@@ -0,0 +1,14 @@
+import { Pickaxe } from 'lucide-react'
+
+export default function Store() {
+  return (
+    <main className="flex flex-col items-center justify-center h-screen gap-4">
+      <Pickaxe className="w-16 h-16" />
+      <h1 className="text-5xl font-semibold">Em construção</h1>
+      <p>
+        Esta página ainda está em desenvolvimento, logo você poderar acessar
+        produtos com ótimo custo e benefício.
+      </p>
+    </main>
+  )
+}
