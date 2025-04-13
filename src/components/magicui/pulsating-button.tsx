c2c8ffbe0f0503a14af06d691eab84da68642c27
willamessantos <willamessantos@live.com>
2025-04-13 09:57:42 -0300

    update page course n8n

--- src/components/magicui/pulsating-button.tsx
+++ src/components/magicui/pulsating-button.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { cn } from "@/lib/utils";
+import React from 'react'
+import { cn } from '@/lib/utils'
 
 interface PulsatingButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  pulseColor?: string;
-  duration?: string;
+  pulseColor?: string
+  duration?: string
 }
 
 export const PulsatingButton = React.forwardRef<
@@ -15,8 +15,8 @@ export const PulsatingButton = React.forwardRef<
     {
       className,
       children,
-      pulseColor = "#0096ff",
-      duration = "1.5s",
+      pulseColor = '#0096ff',
+      duration = '1.5s',
       ...props
     },
     ref,
@@ -25,13 +25,13 @@ export const PulsatingButton = React.forwardRef<
       <button
         ref={ref}
         className={cn(
-          "relative flex cursor-pointer items-center justify-center rounded-lg bg-primary px-4 py-2 text-center text-primary-foreground",
+          'relative flex cursor-pointer items-center justify-center rounded-lg bg-primary px-4 py-2 text-center text-primary-foreground',
           className,
         )}
         style={
           {
-            "--pulse-color": pulseColor,
-            "--duration": duration,
+            '--pulse-color': pulseColor,
+            '--duration': duration,
           } as React.CSSProperties
         }
         {...props}
@@ -39,8 +39,8 @@ export const PulsatingButton = React.forwardRef<
         <div className="relative z-10">{children}</div>
         <div className="absolute left-1/2 top-1/2 size-full -translate-x-1/2 -translate-y-1/2 animate-pulse rounded-lg bg-inherit" />
       </button>
-    );
+    )
   },
-);
+)
 
-PulsatingButton.displayName = "PulsatingButton";
+PulsatingButton.displayName = 'PulsatingButton'
